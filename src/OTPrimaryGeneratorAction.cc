c8a931da693a78b31085d83ccdd1d961c0e5322d
ejungwoo <phyjics@gmail.com>
2019-04-08 21:02:56 +0900

    Using GPS(General Particle Source)

--- src/OTPrimaryGeneratorAction.cc
+++ src/OTPrimaryGeneratorAction.cc
@@ -4,7 +4,7 @@
 #include "G4LogicalVolume.hh"
 #include "G4Box.hh"
 #include "G4RunManager.hh"
-#include "G4ParticleGun.hh"
+#include "G4GeneralParticleSource.hh"
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
 #include "G4SystemOfUnits.hh"
@@ -13,17 +13,7 @@
 OTPrimaryGeneratorAction::OTPrimaryGeneratorAction()
 : G4VUserPrimaryGeneratorAction()
 {
-  G4int n_particle = 1;
-  fParticleGun = new G4ParticleGun(n_particle);
-
-  G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
-  G4String particleName;
-  G4ParticleDefinition* particle
-    = particleTable -> FindParticle(particleName = "proton");
-
-  fParticleGun -> SetParticleDefinition(particle);
-  fParticleGun -> SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
-  fParticleGun -> SetParticleEnergy(40.*MeV);
+  fParticleGun = new G4GeneralParticleSource();
 }
 
 OTPrimaryGeneratorAction::~OTPrimaryGeneratorAction()
